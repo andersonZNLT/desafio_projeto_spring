cc3ba5c46c86320ec199f0e3b88d19339e976c1e
andersonZNLT <anderson.znlt@gmail.com>
2022-08-03 21:38:27 -0300

    controller, model e service

---
+++ src/main/java/one/digitalinnovation/proj/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.proj.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
