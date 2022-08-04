3b84bd019ec2486e95926f0d59165cb84bc0c95b
andersonZNLT <anderson.znlt@gmail.com>
2022-08-03 21:12:47 -0300

    primiro commit

---
+++ src/test/java/one/digitalinnovation/proj/DesafioProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
